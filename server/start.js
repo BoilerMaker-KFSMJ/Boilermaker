6f99b0a13066713195a28966837b21bba34c2615
Fara Woolf <fbwoolf@gmail.com>
2017-07-24 14:05:47 -0500

    feature: setup index.js, webpack, start script and ReactDOM render

--- server/start.js
+++ server/start.js
@@ -2,6 +2,8 @@ const path = require('path')
 const express = require('express')
 const app = express()
 
+const pkg = require('../package.json')
+
 // you'll of course want static middleware so your browser can request things like your 'bundle.js'
 app.use(express.static(path.join(__dirname, '../assets')))
 
@@ -13,3 +15,11 @@ app.use(express.static(path.join(__dirname, '../assets')))
 app.get('*', function (req, res, next) {
   res.sendFile(path.join(__dirname, '../assets/index.html'))
 })
+
+const server = app.listen(
+    process.env.PORT || 1337,
+    () => {
+      console.log(`--- Started HTTP Server for ${pkg.name} ---`)
+      console.log(`Listening on ${JSON.stringify(server.address())}`)
+    }
+ )
