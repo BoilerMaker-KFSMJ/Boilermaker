80acb826b6f2944b3f5241c7d5903f49f80b92bf
Fara Woolf <fbwoolf@gmail.com>
2017-07-24 16:24:24 -0500

    feature: add scss entry point and import

--- browser/index.js
+++ browser/index.js
@@ -2,12 +2,13 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux';
-import store from './yourStore';
+import { Provider } from 'react-redux'
+import store from './yourStore'
+import '../public/index.scss'
 
 ReactDOM.render(
   <Provider store={store}>
     {/* rest of your app goes here! */}
   </Provider>,
   document.getElementById('app')
-);
+)
